39e3d282f6c6eacb56b02d6251eadd414e3cf804
dennisgrauel <dennis@dennisgrauel.com>
2021-06-01 13:57:35 +1000

    Update default.php

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,11 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Disorganising</title>
+    </head>
+    <body>
+        <h1><?= $site->title() ?></h1>
+        <footer><?= $site->acknowledgement() ?></footer>
+    </body>
+</html>
