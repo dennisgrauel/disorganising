b419a0cf40293725dcb30c6b3f68ae7b990d9511
dennisgrauel <dennis@dennisgrauel.com>
2021-06-02 11:13:23 +1000

    Update default.php

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,10 +2,17 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Disorganising</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?= $site->title() ?></title>
+        <?= css('assets/css/style.css') ?>
+        <link rel="stylesheet" href="https://use.typekit.net/qhr3vpf.css">
     </head>
     <body>
-        <h1><?= $site->title() ?></h1>
+        <header><?= $site->heading() ?></header>
+        <div class="about">
+          <?= $site->about()->toBlocks()->limit(1) ?>
+          <p><a href="#">[more …]</a></p>
+        </div>
         <footer><?= $site->acknowledgement() ?></footer>
     </body>
 </html>
