0697f27ffe37707e7cc1e795ca9781edf48af164
dennisgrauel <dennis@dennisgrauel.com>
2021-06-15 17:17:14 +1000

    Update filter.js

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -7,11 +7,17 @@ function showHide(tag) {
     if (works[i].dataset.type == tag) {
       if (checkbox.checked == true) {
         works[i].classList.remove("hide");
-        console.log('show');
+        works[i].ariaHidden = false;
+        // console.log('show');
       } else {
         works[i].classList.add("hide");
-        console.log('hide');
+        works[i].ariaHidden = true;
+        // console.log('hide');
       }
     }
   }
 }
+
+// Log tab index to console
+
+// document.addEventListener('keyup', function() {console.log(document.activeElement)})
